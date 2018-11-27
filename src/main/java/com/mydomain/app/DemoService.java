b262ad12aa417e1a278d683a8561c64c6e8dd055
lane.cn <lane.cn@gmail.com>
2018-11-27 22:44:20 +0800

    complete spring boot starter

---
+++ src/main/java/com/mydomain/app/DemoService.java
@@ -0,0 +1,39 @@
+package com.mydomain.app;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class DemoService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(DemoService.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Scheduled(fixedDelay = 3000)
+	public void sleep() throws Exception {
+		int n = new Random().nextInt(3000);
+		LOG.debug("n is {}", n);
+
+		try {
+			Thread.sleep(n);
+		} catch (InterruptedException e) {
+			LOG.error("error", e);
+		}
+		
+		LOG.info("sleep for {} millis", n);
+	}
+
+	public void baidu() throws InterruptedException {
+		String html = restTemplate.getForObject("https://www.baidu.com", String.class);
+		LOG.debug("html: {}", html);
+	}
+
+}
